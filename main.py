d6ffe54eb03aa2674e04df42bc57a7e36d9b19d4
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-30 12:23:09 +0300

    Введение шаблонов

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Про Flask")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title="О сайте")
 
 if __name__ == "__main__":
     app.run(debug=True)
