aafe92f2a50f07ecec478b9c502fe0056be6dcef
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-30 16:27:34 +0300

    Работа с шаблонами

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
+
 @app.route("/")
 def index():
-    return render_template('index.html', title="Про Flask")
+    return render_template('index.html', menu=menu)
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title="О сайте")
+    return render_template('about.html',title="О сайте", menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
